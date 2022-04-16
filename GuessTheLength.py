e8b9e20b0522799eccfb58784ab9d6904a157a75
BerraFFS <99559718+BerraFFS@users.noreply.github.com>
2022-04-16 13:27:04 +0200

    Update GuessTheLength.py

--- GuessTheLength.py
+++ GuessTheLength.py
@@ -1,21 +1,24 @@
 from random import randint
 
-cor = randint(1, 10)
+res = "y"
 
-x = 5
+while res == "y" or res == "Y":
+    cor = randint(1, 10)
+    x = 5
+    while x > 0:
+        print("You have", str(x), "attempts left")
+        ans = input("How tall is the giraffes neck?: ")
+        if int(ans) == cor:
+            print("You won!")
+            x = 0
+        elif int(ans) != cor:
+            x -= 1
+            if cor < int(ans):
+                print("That's too big!")
+            elif cor > int(ans):
+                print("That's too small")
+    if int(ans) != cor:
+        print("You lost :(")
+    res = input("Would you like to continue? (y/n): ")
 
-while x > 0:
-    print("You have", str(x), "attempts left")
-    ans = input("How tall is the giraffes neck?: ")
-    if int(ans) == cor:
-        print("You won!")
-        break
-    elif int(ans) != cor:
-        x -= 1
-        if cor < int(ans):
-            print("That's too big!")
-        elif cor > int(ans):
-            print("That's too small")
-if int(ans) != cor:
-    print("You lost :(")
 #BerraFFS
